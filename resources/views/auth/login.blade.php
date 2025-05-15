04cfe8428429cb8b31b7068744a9704b81487487
Jeriel Sanao <sanaojeriellian@gmail.com>
2025-05-15 20:36:54 +0800

    Remove remember me checkbox on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,38 +12,21 @@
         </div>
 
         <!-- email -->
-        <div>
+        <div class="mt-5">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" name="email" type="email" :value="old('email')" required autofocus autocomplete="email" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- password -->
-        <div class="mt-4">
+        <div class="mt-4 mb-5">
             <x-input-label for="password" :value="__('Password')" />
             <x-text-input id="password" name="password" type="password" required autocomplete="current-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
         <div class="mt-4">
-            <label for="remember_me" class="form-check-label inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
-                <span class="ms-2">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-
-            {{--             
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif 
-            --}}
-
-            <x-primary-button class="mt-3 gap-2 rounded">
+            <x-primary-button class="mt-5 gap-2 rounded">
                 <span class="material-icons-outlined">login</span>
                 {{ __('Log in') }}
             </x-primary-button>
