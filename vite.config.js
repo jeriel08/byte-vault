c13cf9d496d5f0be4d7959a26911e18017f60ce0
Jeriel Sanao <sanaojeriellian@gmail.com>
2025-05-15 21:02:17 +0800

    Add all CSS files on the vite build and add public/build

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,17 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
                 "resources/css/account-manager-style.css",
-                "resources/css/product-style.css",
+                "resources/css/app.css",
+                "resources/css/dashboard-style.css",
+                "resources/css/inventory-report-style.css",
+                "resources/css/login-style.css",
                 "resources/css/orders-style.css",
+                "resources/css/pos-sales-styling.css",
+                "resources/css/pos-styling.css",
+                "resources/css/product-style.css",
+                "resources/js/app.js",
+                "resources/js/bootstrap.js",
             ],
             refresh: true,
         }),
